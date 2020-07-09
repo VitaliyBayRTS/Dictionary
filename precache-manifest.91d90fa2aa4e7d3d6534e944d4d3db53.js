4436d7db2c513d2698283c1d01ea5c263bcba651
Vitaliy <vitaliygbbay@gmail,com>
2020-07-09 13:45:15 +0200

    Updates

---
+++ precache-manifest.91d90fa2aa4e7d3d6534e944d4d3db53.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447ea4fcacc15e36198ec1c94ad547b4",
+    "url": "/Dictionary/index.html"
+  },
+  {
+    "revision": "3dc894a6de599589cd07",
+    "url": "/Dictionary/static/css/main.b537297d.chunk.css"
+  },
+  {
+    "revision": "5288446e633fa3a9406f",
+    "url": "/Dictionary/static/js/2.bc1ce2b9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Dictionary/static/js/2.bc1ce2b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc894a6de599589cd07",
+    "url": "/Dictionary/static/js/main.0010bc27.chunk.js"
+  },
+  {
+    "revision": "a683ceb020459f3d4d7e",
+    "url": "/Dictionary/static/js/runtime-main.607ef3ff.js"
+  },
+  {
+    "revision": "e31f33396b0523779c66e4a0d2670fc6",
+    "url": "/Dictionary/static/media/logo.e31f3339.png"
+  },
+  {
+    "revision": "4f0bf0b1ab2026a4dc975c54a7c71670",
+    "url": "/Dictionary/static/media/main_bg.4f0bf0b1.png"
+  },
+  {
+    "revision": "c2976018c2010be8a81347c965085ad8",
+    "url": "/Dictionary/static/media/yesNoBg.c2976018.png"
+  }
+]);
